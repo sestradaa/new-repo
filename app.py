006f44b00737602806c94275eaf30f9dece8ff1d
sestradaa <sestradaa2015@hotmail.com>
2024-10-27 09:59:39 -0500

    docs:clase2

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
+#render_template renderiza un template(pagina html), para
+#eso busca una carpeta q se llame template
 
+#creando instancia
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("base.html")
 
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
 
 
+#iniciando el servidor, debug true hace q el codigo 
+# se modifique solo en el navegador
 if __name__ == '__main__':
     app.run(debug=True)
